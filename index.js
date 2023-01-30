aa1a75b9b83cbdc608b0ee6840d1c1352b70b3db
kmokwena <mokwenakw@gmail.com>
2023-01-30 17:09:22 +0200

    improved css

--- index.js
+++ index.js
@@ -6,3 +6,12 @@
  *            clear the display
  *            equals
  */
+
+const clearButton = document.getElementById("clear");
+const display = document.getElementById("display");
+
+clearButton.addEventListener("click", clear);
+
+function clear() {
+  display.innerHTML = "0";
+}
